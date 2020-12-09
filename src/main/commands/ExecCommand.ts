a1a0d1a949ccd92b5bfe2b774e1f3c4948ca4a09
liangguangming <liangguangming@coding.net>
2020-12-09 21:08:24 +0800

    feat: default shell

--- src/main/commands/ExecCommand.ts
+++ src/main/commands/ExecCommand.ts
@@ -37,8 +37,8 @@ export default class ExecCommand implements ICommand {
     }
     const podName = (resArr as Array<Resource>)[0].metadata.name;
     const kubeconfigPath = fileStore.get(CURRENT_KUBECONFIG_FULLPATH);
-    const command = `kubectl exec -it ${podName} -c nocalhost-dev --kubeconfig ${kubeconfigPath} -- /bin/sh`;
-    const terminalDisposed = host.invokeInNewTerminal(command, podName);
+    const command = `kubectl exec -it ${podName} -c nocalhost-dev --kubeconfig ${kubeconfigPath}`;
+    const terminalDisposed = host.invokeInNewTerminal(command, podName, true);
     host.pushDebugDispose(terminalDisposed);
     host.showInformationMessage("DevSpace terminal Opened");
     host.log("", true);
@@ -81,8 +81,8 @@ export default class ExecCommand implements ICommand {
       return;
     }
     const kubeconfigPath = fileStore.get(CURRENT_KUBECONFIG_FULLPATH);
-    const command = `kubectl exec -it ${podName} -c ${containerName} --kubeconfig ${kubeconfigPath} -- /bin/sh`;
-    const terminalDisposed = host.invokeInNewTerminal(command, podName);
+    const command = `kubectl exec -it ${podName} -c nocalhost-dev --kubeconfig ${kubeconfigPath}`;
+    const terminalDisposed = host.invokeInNewTerminal(command, podName, true);
     host.pushDebugDispose(terminalDisposed);
     host.log("open container end", true);
     host.log("", true);
