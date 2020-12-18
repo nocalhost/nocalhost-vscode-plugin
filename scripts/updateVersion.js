f952491d4d1685abd88baf95be21e7a09f1ca6e6
chenjunqing <chenjunqing@coding.net>
2020-12-18 14:54:16 +0800

    feat(scripts/updateVersion.js): update the version of package.json before release new package

---
+++ scripts/updateVersion.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+let version = process.env.VERSION;
+console.log("> update the version to: ", version);
+
+if (!version) return;
+
+const matched = version.match(/\d+\.\d+\.\d+/);
+if (!matched || matched.length !== 1) return;
+
+version = matched[0];
+
+const packageJsonUri = path.resolve(__dirname, "../package.json");
+const packageJson = JSON.parse(
+  fs.readFileSync(packageJsonUri, { encoding: "utf8" })
+);
+
+packageJson.version = version;
+
+fs.unlinkSync(packageJsonUri);
+fs.writeFileSync(packageJsonUri, JSON.stringify(packageJson));
