187b865dd9e0287e29b85f30fcac85763966cc03
liangguangming <gmliangd@163.com>
2020-12-23 13:47:21 +0800

    add port-forward in pod

--- src/main/commands/PortForwardCommand.ts
+++ src/main/commands/PortForwardCommand.ts
@@ -7,6 +7,8 @@ import host from "../host";
 import * as kubectl from "../ctl/kubectl";
 import { KubernetesResourceNode } from "../nodes/abstract/KubernetesResourceNode";
 import { Resource } from "../nodes/types/resourceType";
+import { ControllerResourceNode } from "../nodes/workloads/controllerResources/ControllerResourceNode";
+import { Pod } from "../nodes/workloads/pod/Pod";
 
 export default class PortForwardCommand implements ICommand {
   command: string = PORT_FORWARD;
@@ -18,28 +20,36 @@ export default class PortForwardCommand implements ICommand {
       host.showWarnMessage("A task is running, please try again later");
       return;
     }
-    const kind = node.resourceType;
-    const name = node.name;
-    const appNode = node.getAppNode();
-    const resArr = await kubectl.getControllerPod(
-      appNode.getKUbeconfigPath(),
-      kind,
-      name
-    );
-    if (resArr && resArr.length <= 0) {
-      host.showErrorMessage("Not found pod");
+    let podName: string | undefined;
+    if (node instanceof ControllerResourceNode) {
+      const kind = node.resourceType;
+      const name = node.name;
+      const resArr = await kubectl.getControllerPod(
+        node.getKubeConfigPath(),
+        kind,
+        name
+      );
+      if (resArr && resArr.length <= 0) {
+        host.showErrorMessage("Not found pod");
+        return;
+      }
+      const podNameArr = (resArr as Array<Resource>).map((res) => {
+        return res.metadata.name;
+      });
+      podName = podNameArr[0];
+      if (podNameArr.length > 1) {
+        podName = await vscode.window.showQuickPick(podNameArr);
+      }
+      if (!podName) {
+        return;
+      }
+    } else if (node instanceof Pod) {
+      podName = node.name;
+    } else {
+      host.showInformationMessage("Not support the kind!");
       return;
     }
-    const podNameArr = (resArr as Array<Resource>).map((res) => {
-      return res.metadata.name;
-    });
-    let podName: string | undefined = podNameArr[0];
-    if (podNameArr.length > 1) {
-      podName = await vscode.window.showQuickPick(podNameArr);
-    }
-    if (!podName) {
-      return;
-    }
+
     let portMap: string | undefined = "";
     portMap = await vscode.window.showInputBox({
       placeHolder: "eg: 1234:1234",
