cb8aad70a99bb80a0d3fa3b8c134145eb30a46f1
chenjunqing <chenjunqing@coding.net>
2021-01-15 17:24:52 +0800

    feat(webview integration): add snapshot tester

--- src/renderer/modules/Logs/__tests__/Logs.test.tsx
+++ src/renderer/modules/Logs/__tests__/Logs.test.tsx
@@ -1,25 +1,59 @@
 import React from "react";
 import { ReactWrapper } from "enzyme";
 import Logs from "../index";
-import Landing from "../../Landing";
-import NocalhostTester from "../../../utils/NocalhostTester";
+import NocalhostTester, {
+  NocalhostSnapshotWrapper,
+} from "../../../utils/NocalhostTester";
 import { Actions } from "../../../store/actions/actions.types";
-import { redirect } from "../../../store/actions";
+import { redirect, updateLogs } from "../../../store/actions";
 
 describe("Test <Logs />", () => {
   it("should display no content", (done) => {
-    const tester: NocalhostTester = new NocalhostTester(
-      (dispatch: React.Dispatch<Actions>) => {
+    const tester: NocalhostTester = new NocalhostTester({
+      mounted(dispatch: React.Dispatch<Actions>) {
         dispatch(redirect("/logs"));
-      }
-    );
+      },
+    });
 
     tester.run(() => {
       const wrapper: ReactWrapper | null = tester.getWrapper();
-      console.log(wrapper.html());
       expect(wrapper.find(Logs)).toHaveLength(1);
-      expect(wrapper.find(Landing)).toHaveLength(0);
+      expect(wrapper.text()).toEqual("No Content.");
       done();
     });
+
+    const snapshotWrapper: NocalhostSnapshotWrapper = tester.getSnapshotWrapper();
+    expect(snapshotWrapper).toMatchSnapshot();
+  });
+
+  it("should disploy logs", (done) => {
+    const tester: NocalhostTester = new NocalhostTester({
+      mounted(dispatch: React.Dispatch<Actions>) {
+        dispatch(redirect("/logs"));
+        dispatch(
+          updateLogs({
+            id: "logs-test-id",
+            items: ["AAA", "BBB", "CCC"],
+          })
+        );
+      },
+      mock() {
+        window.HTMLElement.prototype.scrollIntoView = jest.fn();
+      },
+    });
+
+    tester.run(() => {
+      const wrapper: ReactWrapper | null = tester.getWrapper();
+      expect(wrapper.find(Logs)).toHaveLength(1);
+      expect(wrapper.exists("ul")).toBeTruthy();
+      expect(wrapper.find("ul").children().length).toEqual(3);
+      expect(wrapper.find("ul").childAt(0).text()).toEqual("AAA");
+      expect(wrapper.find("ul").childAt(1).text()).toEqual("BBB");
+      expect(wrapper.find("ul").childAt(2).text()).toEqual("CCC");
+      done();
+    });
+
+    const snapshotWrapper: NocalhostSnapshotWrapper = tester.getSnapshotWrapper();
+    expect(snapshotWrapper).toMatchSnapshot();
   });
 });
