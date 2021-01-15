9678d7dd11f3338a5dab85f3e933d544ec8e40a9
chenjunqing <chenjunqing@coding.net>
2021-01-15 15:31:28 +0800

    fix(#634): open config in one tab on command: edit service command

--- src/main/commands/EditServiceConfigCommand.ts
+++ src/main/commands/EditServiceConfigCommand.ts
@@ -21,9 +21,9 @@ export default class EditServiceConfigCommand implements ICommand {
     const uri = vscode.Uri.parse(
       `NocalhostRW://nh/config/app/${appNode.name}/services/${
         node.name
-      }.yaml?id=${node.getNodeStateId()}&&time=${new Date()}`
+      }.yaml?id=${node.getNodeStateId()}&time=${+new Date()}`
     );
     let doc = await vscode.workspace.openTextDocument(uri);
-    vscode.window.showTextDocument(doc, { preview: false });
+    vscode.window.showTextDocument(doc, { preview: true });
   }
 }
