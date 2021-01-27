b66cd6170329c7ed9a556bbbbceef90f27a2cc42
chenjunqing <chenjunqing@coding.net>
2021-01-27 14:59:02 +0800

    feat: data center add describe, config info

--- src/main/common/DataCenter/index.ts
+++ src/main/common/DataCenter/index.ts
@@ -1,15 +1,25 @@
 import * as yaml from "yaml";
 import { getApplication } from "../../api";
-import Application from "./model/Application";
-import ApplicationContext from "./model/ApplicationContext";
-import ApplicationMeta from "./model/ApplicationMeta";
+import {
+  IApplication,
+  IApplicationContext,
+  IApplicationConfig,
+  IApplicationConfigService,
+  IApplicationDescribe,
+  IApplicationDescribeSvcProfile,
+  IApplicationDescribeSvcProfileRawConfig,
+  IApplicationMeta,
+  IApplicationMetaSvcProfile,
+} from "./index.types";
 import * as shell from "../../ctl/shell";
 import services, { ServiceResult } from "./services";
 import { DATA_CENTER_INTERVAL_MS } from "../../constants";
 
 interface IDataStore {
-  applications: Application[];
-  applicationMetas: Map<string, ApplicationMeta>;
+  applications: IApplication[];
+  applicationMetas: Map<string, IApplicationMeta>;
+  applicationDescribes: Map<string, IApplicationDescribe>;
+  applicationConfigs: Map<string, IApplicationConfig>;
 }
 export interface IExecCommandResult {
   success: boolean;
@@ -17,7 +27,7 @@ export interface IExecCommandResult {
 }
 let instance: DataCenter | null = null;
 
-export type ApplicationListener = (applications?: Application[]) => void;
+export type ApplicationListener = (applications?: IApplication[]) => void;
 
 export default class DataCenter {
   public static getInstance(timeout = DATA_CENTER_INTERVAL_MS): DataCenter {
@@ -38,8 +48,10 @@ export default class DataCenter {
   }
 
   private dataStore: IDataStore = {
-    applications: [],
-    applicationMetas: new Map(),
+    applications: new Array<IApplication>(),
+    applicationMetas: new Map<string, IApplicationMeta>(),
+    applicationDescribes: new Map<string, IApplicationDescribe>(),
+    applicationConfigs: new Map<string, IApplicationConfig>(),
   };
   private listeners: ApplicationListener[] = [];
 
@@ -49,33 +61,36 @@ export default class DataCenter {
 
   private async setApplications(timeout?: number): Promise<void> {
     const results: any[] = await getApplication();
-    const applications: Promise<Application>[] = results.map(
+    const applications: Promise<IApplication>[] = results.map(
       async (result: any) => {
         const contextObj: any = JSON.parse(result.context || "{}");
-        const context: ApplicationContext = new ApplicationContext(
-          contextObj.source || "",
-          contextObj["install_type"] || "rawManifest",
-          contextObj["application_name"] || "",
-          contextObj["application_url"] || "",
-          contextObj["resource_dir"] || ["manifest/templates"],
-          contextObj["application_config_path"] || "",
-          contextObj["nocalhost_config"] || ""
-        );
+        const context: IApplicationContext = {
+          source: contextObj.source || "",
+          installType: contextObj["install_type"] || "rawManifest",
+          applicationName: contextObj["application_name"] || "",
+          applicationURL: contextObj["application_url"] || "",
+          resourceDir: contextObj["resource_dir"] || ["manifest/templates"],
+          applicationConfigPath: contextObj["application_config_path"] || "",
+          nocalhostConfig: contextObj["nocalhost_config"] || "",
+        };
         await this.fetchApplicationMeta(context.applicationName);
-        return new Application(
-          result.id,
-          context,
-          result.status,
-          result.installStatus,
-          result.kubeconfig || "",
-          result.cpu,
-          result.memory,
-          result.namespace || "",
-          result.clusterId,
-          result.devspaceId,
-          result.spaceName || "",
-          result.storageClass || ""
-        );
+        await this.fetchApplicationDescribe(context.applicationName);
+        await this.fetchApplicationConfig(context.applicationName);
+        return {
+          id: result.id,
+          context: context,
+          status: result.status,
+          installStatus: result["install_status"],
+          kubeConfig: result.kubeconfig || "",
+          cpu: result.cpu,
+          memory: result.memory,
+          namespace: result.namespace || "",
+          clusterId: result["cluster_id"],
+          devspaceId: result["devspace_id"],
+          spaceName: result["space_name"] || "",
+          storageClass: result["storage_class"] || "",
+          devStartAppendCommand: result["dev_start_append_command"] || "",
+        };
       }
     );
     this.dataStore.applications = await Promise.all(applications);
@@ -84,11 +99,13 @@ export default class DataCenter {
         listener(this.dataStore.applications);
       });
     }
-    if (timeout) {
-      setTimeout(() => {
-        this.setApplications(timeout);
-      }, timeout);
-    }
+
+    // TODO: DO NOT DELETE, FOR: [auto refresh]
+    // if (timeout) {
+    //   setTimeout(() => {
+    //     this.setApplications(timeout);
+    //   }, timeout);
+    // }
   }
 
   private async fetchApplicationMeta(applicationName: string): Promise<void> {
@@ -99,34 +116,190 @@ export default class DataCenter {
     if (rawData) {
       const data: any = yaml.parse(rawData);
       if (typeof data !== "string") {
-        const meta: ApplicationMeta = new ApplicationMeta(
-          data.name || "",
-          data.releasename || "",
-          data.namespace || "",
-          data.kubeconfig || "",
-          data.dependencyConfigMapName || "",
-          data.appType || "",
-          data.svcProfile || [],
-          data.installed,
-          data.resourcePath || []
-        );
+        const svcProfile: IApplicationMetaSvcProfile[] =
+          data.svcProfile && Array.isArray(data.svcProfile)
+            ? data.svcProfile.map((profile: any) => ({
+                name: profile.name || "",
+                serviceType: profile.serviceType || "",
+                gitUrl: profile.gitUrl || "",
+                devContainerImage: profile.devContainerImage || "",
+                workDir: profile.workDir || "",
+                syncDirs: profile.syncDirs || [],
+                devPorts: profile.devPorts || [],
+                developing: profile.developing || false,
+                portForwarded: profile.portForwarded || false,
+                syncing: profile.syncing || false,
+                localAbsoluteSyncDirFromDevStartPlugin:
+                  profile.localAbsoluteSyncDirFromDevStartPlugin || [],
+                devPortList: profile.devPortList || [],
+                syncFilePattern: profile.syncFilePattern || [],
+                ignoreFilePattern: profile.ignoreFilePattern || [],
+              }))
+            : [];
+        const meta: IApplicationMeta = {
+          name: data.name || "",
+          releasename: data.releasename || "",
+          namespace: data.namespace || "",
+          kubeConfig: data.kubeconfig || "",
+          dependencyConfigMapName: data.dependencyConfigMapName || "",
+          appType: data.appType || "",
+          svcProfile: svcProfile,
+          installed: data.installed || false,
+          resourcePath: data.resourcePath || [],
+        };
         this.setApplicationMeta(applicationName, meta);
       }
     }
   }
 
-  private setApplicationMeta(applicationName: string, meta: ApplicationMeta) {
+  private setApplicationMeta(
+    applicationName: string,
+    meta: IApplicationMeta
+  ): void {
     this.dataStore.applicationMetas.set(applicationName, meta);
   }
 
-  public getApplications(): Application[] {
+  private async fetchApplicationDescribe(
+    applicationName: string
+  ): Promise<void> {
+    const result: ServiceResult = await services.describeApplication(
+      applicationName
+    );
+    const rawData: string = result.success ? result.value : "";
+    if (rawData) {
+      const data: any = yaml.parse(rawData);
+      if (typeof data !== "string") {
+        const svcProfile: IApplicationDescribeSvcProfile[] = [];
+        if (data.svcProfile && Array.isArray(data.svcProfile)) {
+          data.svcProfile.forEach((profile: any) => {
+            const rawConfig: IApplicationDescribeSvcProfileRawConfig = {
+              name: profile.rawConfig.name || "",
+              serviceType: profile.rawConfig.serviceType || "",
+              gitUrl: profile.rawConfig.gitUrl || "",
+              devContainerImage: profile.rawConfig.devContainerImage || "",
+              workDir: profile.rawConfig.workDir || "",
+              persistentVolumeDirs:
+                profile.rawConfig.persistentVolumeDirs || [],
+              runCommand: profile.rawConfig.runCommand || [],
+              hotReloadRunCommand: profile.rawConfig.hotReloadRunCommand || [],
+              devContainerShell: profile.rawConfig.devContainerShell || "",
+              ignores: profile.rawConfig.ignores || [],
+              devContainerResources:
+                profile.rawConfig.devContainerResources || "",
+              devPorts: profile.rawConfig.devPorts || [],
+              dependJobsLabelSelector:
+                profile.rawConfig.dependJobsLabelSelector || [],
+              syncFilePattern: profile.rawConfig.syncFilePattern || [],
+              ignoreFilePattern: profile.rawConfig.ignoreFilePattern || [],
+            };
+            svcProfile.push({
+              rawConfig,
+              actualName: profile.actualName || "",
+              developing: profile.developing || false,
+              portForwarded: profile.portForwarded || false,
+              syncing: profile.syncing || false,
+              remoteSyncthingPort: profile.remoteSyncthingPort,
+              remoteSyncthingGUIPort: profile.remoteSyncthingGUIPort,
+              syncthingSecret: profile.syncthingSecret || "",
+              localSyncthingPort: profile.localSyncthingPort,
+              localSyncthingGUIPort: profile.localSyncthingGUIPort,
+              localAbsoluteSyncDirFromDevStartPlugin:
+                profile.localAbsoluteSyncDirFromDevStartPlugin || [],
+              devPortList: profile.devPortList || [],
+              portForwardStatusList: profile.portForwardStatusList || [],
+              portForwardPidList: profile.portForwardPidList || [],
+              syncFilePattern: profile.syncFilePattern || [],
+              ignoreFilePattern: profile.ignoreFilePattern || [],
+            });
+          });
+        }
+        const describeInfo: IApplicationDescribe = {
+          name: data.name || "",
+          releasename: data.releasename || "",
+          namespace: data.namespace || "",
+          kubeConfig: data.kubeconfig || "",
+          dependencyConfigMapName: data.dependencyConfigMapName || "",
+          appType: data.appType || "",
+          svcProfile: svcProfile,
+          installed: data.installed || false,
+          resourcePath: data.resourcePath || [],
+        };
+        this.setApplicationDescribe(applicationName, describeInfo);
+      }
+    }
+  }
+
+  private setApplicationDescribe(
+    applicationName: string,
+    describeInfo: IApplicationDescribe
+  ): void {
+    this.dataStore.applicationDescribes.set(applicationName, describeInfo);
+  }
+
+  private async fetchApplicationConfig(applicationName: string): Promise<void> {
+    const result: ServiceResult = await services.fetchApplicationConfig(
+      applicationName
+    );
+    const rawData: string = result.success ? result.value : "";
+    if (rawData) {
+      const data: any = yaml.parse(rawData);
+      console.log(111, data);
+      if (typeof data !== "string") {
+        const services: IApplicationConfigService[] =
+          data.services && Array.isArray(data.services)
+            ? data.services.map((service: any) => ({
+                name: service.name || "",
+                serviceType: service.serviceType || "",
+                gitUrl: service.gitUrl || "",
+                devContainerImage: service.devContainerImage || "",
+                workDir: service.workDir || "",
+                persistentVolumeDirs: service.persistentVolumeDirs || [],
+                runCommand: service.runCommand || [],
+                hotReloadRunCommand: service.hotReloadRunCommand || [],
+                devContainerShell: service.devContainerShell || "",
+                ignores: service.ignores || [],
+                devContainerResources: service.devContainerResources || "",
+                devPorts: service.devPorts || [],
+                dependJobsLabelSelector: service.dependJobsLabelSelector || [],
+                syncFilePattern: service.syncFilePattern || [],
+                ignoreFilePattern: service.ignoreFilePattern || [],
+              }))
+            : [];
+        const config: IApplicationConfig = {
+          services,
+        };
+        this.setApplicationConfig(applicationName, config);
+      }
+    }
+  }
+
+  private setApplicationConfig(
+    applicationName: string,
+    config: IApplicationConfig
+  ): void {
+    this.dataStore.applicationConfigs.set(applicationName, config);
+  }
+
+  public getApplications(): IApplication[] {
     return this.dataStore.applications;
   }
 
   public getApplicationMeta(
     applicationName: string
-  ): ApplicationMeta | undefined {
+  ): IApplicationMeta | undefined {
     return this.dataStore.applicationMetas.get(applicationName);
+  }
+
+  public getApplicationDescribe(
+    applicationName: string
+  ): IApplicationDescribe | undefined {
+    return this.dataStore.applicationDescribes.get(applicationName);
+  }
+
+  public getApplicationConfig(
+    applicationName: string
+  ): IApplicationConfig | undefined {
+    return this.dataStore.applicationConfigs.get(applicationName);
   }
 
   public addListener(listener: ApplicationListener): void {
