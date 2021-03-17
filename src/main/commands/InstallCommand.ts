4d068f1b45010561886a1deb0f88adb255477b9a
liangguangming <gmliangd@163.com>
2021-03-17 21:25:06 +0800

    feat(install): remove invalid code

--- src/main/commands/InstallCommand.ts
+++ src/main/commands/InstallCommand.ts
@@ -165,23 +165,8 @@ export default class InstallCommand implements ICommand {
         }
       }
     }
-    state.setAppState(appNode.name, "installing", true, {
-      refresh: true,
-      nodeStateId: appNode.getNodeStateId(),
-    });
 
-    await this.startInstall(
-      appNode,
-      values,
-      valuesStr,
-      refOrVersion,
-      local
-    ).finally(() => {
-      state.deleteAppState(appNode.name, "installing", {
-        refresh: true,
-        nodeStateId: appNode.getNodeStateId(),
-      });
-    });
+    await this.startInstall(appNode, values, valuesStr, refOrVersion, local);
     productPagePort = this.productPagePort;
     await vscode.commands.executeCommand(REFRESH);
     if (this.isBookInfo(appNode) && productPagePort) {
