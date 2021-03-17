3cc7ea689b0751c28be8c8c710fb20588c6c5c38
liangguangming <gmliangd@163.com>
2021-03-17 21:23:20 +0800

    feat(viewKubeconfig): change view kubeconfig

--- src/main/commands/ViewKubeConfig.ts
+++ src/main/commands/ViewKubeConfig.ts
@@ -5,14 +5,14 @@ import ICommand from "./ICommand";
 import { VIEW_KUBECONFIG } from "./constants";
 import registerCommand from "./register";
 import host from "../host";
-import { AppNode } from "../nodes/AppNode";
+import { DevSpaceNode } from "../nodes/DevSpaceNode";
 
 export default class ViewKubeConfigCommand implements ICommand {
   command: string = VIEW_KUBECONFIG;
   constructor(context: vscode.ExtensionContext) {
     registerCommand(context, this.command, false, this.execCommand.bind(this));
   }
-  async execCommand(node: AppNode) {
+  async execCommand(node: DevSpaceNode) {
     if (!node) {
       host.showWarnMessage("A task is running, please try again later");
       return;
