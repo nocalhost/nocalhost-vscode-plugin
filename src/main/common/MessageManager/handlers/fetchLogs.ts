a0dada63b3764291eb42f51e648eb8c8b1e1f516
liangguangming <gmliangd@163.com>
2021-03-26 15:17:26 +0800

    fix(log): can not display log info

--- src/main/common/MessageManager/handlers/fetchLogs.ts
+++ src/main/common/MessageManager/handlers/fetchLogs.ts
@@ -1,15 +1,11 @@
 import { IMessage } from "..";
 import NocalhostWebviewPanel from "../../../webview/NocalhostWebviewPanel";
-import DataCenter from "../../DataCenter";
-import {
-  IApplicationDescribe,
-  IApplicationMeta,
-} from "../../DataCenter/index.types";
 import services, { ServiceResult } from "../../DataCenter/services";
+import state from "../../../state";
+import { AppNode } from "../../../nodes/AppNode";
 
 export default async function fetchLogs(message: IMessage, id: number) {
   const { payload } = message;
-  const dataCenter = DataCenter.getInstance();
   if (
     !payload ||
     !payload.id ||
@@ -19,30 +15,29 @@ export default async function fetchLogs(message: IMessage, id: number) {
   ) {
     return;
   }
-  const applicationDescribe:
-    | IApplicationDescribe
-    | undefined = dataCenter.getApplicationDescribe(payload.app);
-  if (applicationDescribe) {
-    const kubeConfig: string = applicationDescribe.kubeConfig;
-    const result: ServiceResult = await services.fetchLogs(
-      payload.pod,
-      payload.container,
-      payload.tail,
-      kubeConfig
-    );
-    const content: string = result.success ? result.value : "";
-    const items: string[] = content ? content.split("\n") : [];
-    NocalhostWebviewPanel.postMessage(
-      {
-        type: "logs/update",
-        payload: {
-          logs: {
-            id: payload.id,
-            items,
-          },
+  const appNode = state.getNode(payload.id) as AppNode;
+  if (!appNode) {
+    return;
+  }
+  const kubeConfig = appNode.getKubeConfigPath();
+  const result: ServiceResult = await services.fetchLogs(
+    payload.pod,
+    payload.container,
+    payload.tail,
+    kubeConfig
+  );
+  const content: string = result.success ? result.value : "";
+  const items: string[] = content ? content.split("\n") : [];
+  NocalhostWebviewPanel.postMessage(
+    {
+      type: "logs/update",
+      payload: {
+        logs: {
+          id: payload.id,
+          items,
         },
       },
-      id
-    );
-  }
+    },
+    id
+  );
 }
