9686f4bb7dd4f09f6804369e872f7e4e88b970c7
zhangjian <zhangjian@coding.net>
2021-08-02 11:41:51 +0800

    chore/coding-artifacts

--- scripts/gitVersion.js
+++ scripts/gitVersion.js
@@ -1,16 +1,16 @@
-
 const fs = require("fs");
 const path = require("path");
-const shelljs =require('shelljs');
 
 const packageJsonUri = path.resolve(__dirname, "../package.json");
 const packageJson = JSON.parse(
   fs.readFileSync(packageJsonUri, { encoding: "utf8" })
 );
 
-const gitVer =shelljs.exec("git rev-list --all --count").stdout.replace('\n',"");
+const { PLUGIN_VERSION } = process.env;
 
-const version=`-beta.${gitVer}`;
+console.log("> update the version to: ", PLUGIN_VERSION);
+
+const version = `-beta.${PLUGIN_VERSION}`;
 
 packageJson.version += version;
 
