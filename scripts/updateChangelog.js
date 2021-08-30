83588a4991869cf9b4a44e99ac04b0c2e545bbe5
yuanganping <yuanganping@coding.net>
2021-08-30 17:24:36 +0800

    feat: change log

--- scripts/updateChangelog.js
+++ scripts/updateChangelog.js
@@ -2,8 +2,13 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 
-let updateURL =
-  "https://raw.githubusercontent.com/nocalhost/nocalhost/main/CHANGELOG/CHANGELOG-v0.4.9.md";
+let version = process.env.VERSION || "0.5.1";
+console.log("> update changelog version to: ", version);
+
+let updateURL = `https://raw.githubusercontent.com/nocalhost/nocalhost.github.io/main/docs/changelogs/${version.replace(
+  /(\d\.\d\.)(\d)/,
+  "$1x"
+)}.md`;
 const CHANGELOG_PATH = path.resolve(__dirname, "../CHANGELOG.md");
 
 function readUpdateFile() {
@@ -14,16 +19,20 @@ function readUpdateFile() {
       data += chunk;
     });
     res.on("end", function () {
-      let regex = /\*\*VSCode Plugin\*\*([\w\W]*?)\*\*nhctl\*\*/;
       const header = `# Change Log`;
+
+      const header2 = data.match(/##\s.*?\)/)[0];
+      console.log(header2);
+
+      let regex = /###\sVS\sCode([\w\W]*?)###\sJetBrains/;
       const result = data.match(regex)[1];
-      // const buf = Buffer.from(result);
-      // fs.appendFileSync(CHANGELOG_PATH, buf);
+      console.log(result);
+
       let file = fs
         .readFileSync(CHANGELOG_PATH)
         .toString()
         .replace(/# Change Log/, "");
-      const buf = Buffer.from(header + result + file);
+      const buf = Buffer.from(`${header}\n${header2}${result}${file}`);
       fs.writeFileSync(CHANGELOG_PATH, buf);
     });
   });
