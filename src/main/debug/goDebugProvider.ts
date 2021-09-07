018aec8887717de76c82c15235cca2db738d3a54
zhangjian <zhangjian@coding.net>
2021-09-07 19:25:20 +0800

    debug

--- src/main/debug/goDebugProvider.ts
+++ src/main/debug/goDebugProvider.ts
@@ -1,9 +1,5 @@
 import * as vscode from "vscode";
-import * as path from "path";
-import { spawnSync } from "child_process";
-import { NhctlCommand } from "./../ctl/nhctl";
 import { IDebugProvider } from "./IDebugprovider";
-import host from "../host";
 
 export class GoDebugProvider extends IDebugProvider {
   async startDebug(
@@ -11,9 +7,8 @@ export class GoDebugProvider extends IDebugProvider {
     sessionName: string,
     port: number,
     workDir: string,
-    terminatedCallback: () => any
+    terminatedCallback: Function
   ): Promise<boolean> {
-    // check extension installed
     const debugConfiguration: vscode.DebugConfiguration = {
       name: sessionName,
       type: "go",
@@ -26,31 +21,12 @@ export class GoDebugProvider extends IDebugProvider {
       // trace: "verbose", // check debug step
       // NOT SUPPORT CWD, will occur error
     };
-    const currentFolder = (vscode.workspace.workspaceFolders || []).find(
-      (folder) => folder.name === path.basename(workspaceFolder)
-    );
-    const disposables: vscode.Disposable[] = [];
-    disposables.push(
-      vscode.debug.onDidStartDebugSession((debugSession) => {
-        if (debugSession.name === sessionName) {
-          host.log(
-            "The debug session has started. Your application is ready for you to debug.",
-            true
-          );
-        }
-      })
-    );
 
-    disposables.push(
-      vscode.debug.onDidTerminateDebugSession(async (debugSession) => {
-        if (debugSession.name === sessionName) {
-          disposables.forEach((d) => d.dispose());
-          await terminatedCallback();
-          host.log("Terminated debug session", true);
-        }
-      })
+    return super.startDebugging(
+      workspaceFolder,
+      debugConfiguration,
+      terminatedCallback
     );
-    return await vscode.debug.startDebugging(currentFolder, debugConfiguration);
   }
 
   public async isDebuggerInstalled(): Promise<boolean> {
@@ -59,33 +35,5 @@ export class GoDebugProvider extends IDebugProvider {
     }
 
     return super.installExtension("golang", ["golang.go"]);
-  }
-
-  public killContainerDebugProcess(
-    podName: string,
-    kubeconfigPath: string,
-    execCommand: string[],
-    namespace: string
-  ) {
-    super.killContainerDebugProcess(
-      podName,
-      kubeconfigPath,
-      execCommand,
-      namespace
-    );
-    // kill exec program
-    const index = execCommand.indexOf("exec");
-    if (index < 0) {
-      return;
-    }
-    const searchCommand = execCommand[index + 1];
-    host.log("searchCommand: " + searchCommand, true);
-    const command = `k exec ${podName} -c nocalhost-dev --kubeconfig ${kubeconfigPath} --`;
-    const args = command.split(" ");
-
-    const killCommand = `kill -9 \`ps aux|grep -i '${searchCommand.trim()}'|grep -v grep|awk '{print $2}'\``;
-
-    args.push("bash", "-c", `${killCommand}`);
-    spawnSync(NhctlCommand.nhctlPath, args);
   }
 }
