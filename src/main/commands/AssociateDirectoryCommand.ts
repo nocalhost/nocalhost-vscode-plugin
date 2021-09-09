1be7dba33b57dd0ab73a4fae04e5517f5fef6dd7
yuanganping <yuanganping@coding.net>
2021-09-09 16:08:44 +0800

    fix: change 2 exec associate command

--- src/main/commands/AssociateDirectoryCommand.ts
+++ src/main/commands/AssociateDirectoryCommand.ts
@@ -1,36 +1,30 @@
-import * as vscode from "vscode";
-import * as os from "os";
+import * as vscode from 'vscode'
+import * as os from 'os'
 
-import ICommand from "./ICommand";
-import { ASSOCIATE_LOCAL_DIRECTORY } from "./constants";
-import registerCommand from "./register";
-import { Deployment } from "../nodes/workloads/controllerResources/deployment/Deployment";
-import host from "../host";
-import { associate, getServiceConfig } from "../ctl/nhctl";
+import ICommand from './ICommand'
+import { ASSOCIATE_LOCAL_DIRECTORY } from './constants'
+import registerCommand from './register'
+import { Deployment } from '../nodes/workloads/controllerResources/deployment/Deployment'
+import host from '../host'
+import { associate, getServiceConfig } from '../ctl/nhctl'
 
 export default class AssociateLocalDirectoryCommand implements ICommand {
-  command: string = ASSOCIATE_LOCAL_DIRECTORY;
+  command: string = ASSOCIATE_LOCAL_DIRECTORY
   constructor(context: vscode.ExtensionContext) {
-    registerCommand(context, this.command, false, this.execCommand.bind(this));
+    registerCommand(context, this.command, false, this.execCommand.bind(this))
   }
-  async execCommand(node: Deployment) {
+  async execCommand(node: Deployment, openDir?: boolean) {
     if (!node) {
-      host.showWarnMessage("Failed to get node configs, please try again.");
-      return;
+      host.showWarnMessage('Failed to get node configs, please try again.')
+      return
     }
-    let appName: string, workloadName: string | undefined;
-    appName = node.getAppName();
-    workloadName = node.name;
-    const namespace = node.getNameSpace();
-    const kubeConfigPath = node.getKubeConfigPath();
+    let appName: string, workloadName: string | undefined
+    appName = node.getAppName()
+    workloadName = node.name
+    const namespace = node.getNameSpace()
+    const kubeConfigPath = node.getKubeConfigPath()
 
-    const status = await node.getStatus();
-    if (status === "developing") {
-      host.showWarnMessage(
-        "You are already in DevMode, please exit and try again"
-      );
-      return;
-    }
+    const status = await node.getStatus()
 
     const profile = await getServiceConfig(
       node.getKubeConfigPath(),
@@ -38,14 +32,14 @@ export default class AssociateLocalDirectoryCommand implements ICommand {
       node.getAppName(),
       node.name,
       node.resourceType
-    );
+    )
 
-    const currentUri = host.getCurrentRootPath();
+    const currentUri = host.getCurrentRootPath()
 
     const selectUri = await host.showSelectFolderDialog(
-      "Associate local directory",
+      'Associate local directory',
       vscode.Uri.file(profile.associate || currentUri || os.homedir())
-    );
+    )
     if (selectUri && selectUri.length > 0) {
       await associate(
         kubeConfigPath,
@@ -54,8 +48,12 @@ export default class AssociateLocalDirectoryCommand implements ICommand {
         selectUri[0].fsPath,
         node.resourceType,
         workloadName
-      );
-      host.showInformationMessage("Directory successfully linked");
+      )
+      if (openDir) {
+        vscode.commands.executeCommand('vscode.openFolder', selectUri[0], true)
+      } else {
+        host.showInformationMessage('Directory successfully linked')
+      }
     }
   }
 }
