7ada40f91d5ad7a2aecea579cba9da423e7baac1
yuanganping <yuanganping@coding.net>
2021-09-13 13:49:56 +0800

    fix: associate add containerName

--- src/main/commands/StartDevModeCommand.ts
+++ src/main/commands/StartDevModeCommand.ts
@@ -1,12 +1,12 @@
-import * as vscode from 'vscode';
-import * as os from 'os';
-import { INhCtlGetResult, IDescribeConfig } from '../domain';
-import ICommand from './ICommand';
-import { NhctlCommand } from './../ctl/nhctl';
-import { START_DEV_MODE, SYNC_SERVICE } from './constants';
-import registerCommand from './register';
-import { get as _get } from 'lodash';
-import { opendevSpaceExec } from '../ctl/shell';
+import * as vscode from "vscode";
+import * as os from "os";
+import { INhCtlGetResult, IDescribeConfig } from "../domain";
+import ICommand from "./ICommand";
+import { NhctlCommand } from "./../ctl/nhctl";
+import { START_DEV_MODE, SYNC_SERVICE } from "./constants";
+import registerCommand from "./register";
+import { get as _get } from "lodash";
+import { opendevSpaceExec } from "../ctl/shell";
 import {
   TMP_APP,
   TMP_CONTAINER,
@@ -20,21 +20,21 @@ import {
   TMP_STORAGE_CLASS,
   TMP_WORKLOAD,
   TMP_WORKLOAD_PATH,
-} from '../constants';
-import host, { Host } from '../host';
-import * as path from 'path';
-import git from '../ctl/git';
-import ConfigService from '../service/configService';
-import * as nhctl from '../ctl/nhctl';
-import * as nls from '../../../package.nls.json';
-import { replaceSpacePath } from '../utils/fileUtil';
-import { BaseNocalhostNode, DeploymentStatus } from '../nodes/types/nodeType';
-import { ControllerResourceNode } from '../nodes/workloads/controllerResources/ControllerResourceNode';
-import { appTreeView } from '../extension';
-import messageBus from '../utils/messageBus';
-import logger from '../utils/logger';
-import { existsSync } from 'fs';
-import { getContainer } from '../utils/getContainer';
+} from "../constants";
+import host, { Host } from "../host";
+import * as path from "path";
+import git from "../ctl/git";
+import ConfigService from "../service/configService";
+import * as nhctl from "../ctl/nhctl";
+import * as nls from "../../../package.nls.json";
+import { replaceSpacePath } from "../utils/fileUtil";
+import { BaseNocalhostNode, DeploymentStatus } from "../nodes/types/nodeType";
+import { ControllerResourceNode } from "../nodes/workloads/controllerResources/ControllerResourceNode";
+import { appTreeView } from "../extension";
+import messageBus from "../utils/messageBus";
+import logger from "../utils/logger";
+import { existsSync } from "fs";
+import { getContainer } from "../utils/getContainer";
 
 export interface ControllerNodeApi {
   name: string;
@@ -63,15 +63,15 @@ export default class StartDevModeCommand implements ICommand {
   private node: ControllerNodeApi;
   async execCommand(node: ControllerNodeApi) {
     if (!node) {
-      host.showWarnMessage('Failed to get node configs, please try again.');
+      host.showWarnMessage("Failed to get node configs, please try again.");
       return;
     }
 
     this.node = node;
 
     await NhctlCommand.authCheck({
-      base: 'dev',
-      args: ['start', node.getAppName(), '-t' + node.resourceType, node.name],
+      base: "dev",
+      args: ["start", node.getAppName(), "-t" + node.resourceType, node.name],
       kubeConfigPath: node.getKubeConfigPath(),
       namespace: node.getNameSpace(),
     }).exec();
@@ -79,14 +79,14 @@ export default class StartDevModeCommand implements ICommand {
     if (node instanceof ControllerResourceNode && appTreeView) {
       await appTreeView.reveal(node, { select: true, focus: true });
     }
-    host.log('[start dev] Initializing..', true);
+    host.log("[start dev] Initializing..", true);
     const resource: INhCtlGetResult = await NhctlCommand.get({
       kubeConfigPath: node.getKubeConfigPath(),
       namespace: node.getNameSpace(),
     })
       .addArgumentStrict(node.resourceType, node.name)
-      .addArgument('-a', node.getAppName())
-      .addArgument('-o', 'json')
+      .addArgument("-a", node.getAppName())
+      .addArgument("-o", "json")
       .exec();
 
     const description: IDescribeConfig =
@@ -99,13 +99,13 @@ export default class StartDevModeCommand implements ICommand {
 
     host.log(`[start dev] Container: ${containerName}`, true);
 
-    if (containerName === 'nocalhost-dev') {
+    if (containerName === "nocalhost-dev") {
       let r = await host.showInformationMessage(
         `This container is developing. You may have problem after enter DevMode at the same time. Do you want to continue?`,
         { modal: true },
-        'confirm'
+        "confirm"
       );
-      if (r !== 'confirm') {
+      if (r !== "confirm") {
         return;
       }
     }
@@ -131,29 +131,29 @@ export default class StartDevModeCommand implements ICommand {
     );
     if (!image) {
       const result = await host.showInformationMessage(
-        'Please specify development image',
+        "Please specify development image",
         { modal: true },
-        'Select',
-        'Custom'
+        "Select",
+        "Custom"
       );
       if (!result) {
         return;
       }
-      if (result === 'Select') {
+      if (result === "Select") {
         const images = [
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/java:11',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/ruby:3.0',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/node:14',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/python:3.9',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/golang:1.16',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/perl:latest',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/rust:latest',
-          'nocalhost-docker.pkg.coding.net/nocalhost/dev-images/php:latest',
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/java:11",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/ruby:3.0",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/node:14",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/python:3.9",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/golang:1.16",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/perl:latest",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/rust:latest",
+          "nocalhost-docker.pkg.coding.net/nocalhost/dev-images/php:latest",
         ];
         image = await host.showQuickPick(images);
-      } else if (result === 'Custom') {
+      } else if (result === "Custom") {
         image = await host.showInputBox({
-          placeHolder: 'Please input your image address',
+          placeHolder: "Please input your image address",
         });
         if (!image) {
           return;
@@ -168,7 +168,7 @@ export default class StartDevModeCommand implements ICommand {
       node.name,
       node.resourceType,
       containerName,
-      'image',
+      "image",
       image as string
     );
     if (
@@ -178,7 +178,7 @@ export default class StartDevModeCommand implements ICommand {
       await this.startDevMode(host, appName, node, containerName);
     } else if (destDir) {
       this.saveAndOpenFolder(appName, node, destDir, containerName);
-      messageBus.emit('devstart', {
+      messageBus.emit("devstart", {
         name: appName,
         destDir,
         container: containerName,
@@ -196,7 +196,7 @@ export default class StartDevModeCommand implements ICommand {
 
     const uri = vscode.Uri.file(destDir);
     if (currentUri !== uri.fsPath) {
-      vscode.commands.executeCommand('vscode.openFolder', uri, true);
+      vscode.commands.executeCommand("vscode.openFolder", uri, true);
       this.setTmpStartRecord(
         appName,
         uri.fsPath,
@@ -226,7 +226,7 @@ export default class StartDevModeCommand implements ICommand {
     );
     if (!gitUrl) {
       gitUrl = await host.showInputBox({
-        placeHolder: 'please input your git url',
+        placeHolder: "please input your git url",
       });
     }
     if (gitUrl) {
@@ -234,7 +234,7 @@ export default class StartDevModeCommand implements ICommand {
         canSelectFiles: false,
         canSelectFolders: true,
         canSelectMany: false,
-        title: 'select save directory',
+        title: "select save directory",
       });
       if (saveUris && saveUris.length > 0) {
         destDir = path.resolve(saveUris[0].fsPath, workloadName);
@@ -245,12 +245,12 @@ export default class StartDevModeCommand implements ICommand {
           workloadName,
           wrokloadType,
           containerName,
-          'gitUrl',
+          "gitUrl",
           gitUrl
         );
-        await host.showProgressing('Starting DevMode', async (progress) => {
+        await host.showProgressing("Starting DevMode", async (progress) => {
           progress.report({
-            message: 'cloning code',
+            message: "cloning code",
           });
           await git.clone(host, gitUrl as string, [
             replaceSpacePath(destDir) as string,
@@ -290,15 +290,15 @@ export default class StartDevModeCommand implements ICommand {
   ) {
     let destDir: string | undefined;
     const result = await host.showInformationMessage(
-      nls['tips.clone'],
+      nls["tips.clone"],
       { modal: true },
-      nls['bt.clone'],
-      nls['bt.open.dir']
+      nls["bt.clone"],
+      nls["bt.open.dir"]
     );
     if (!result) {
       return;
     }
-    if (result === nls['bt.clone']) {
+    if (result === nls["bt.clone"]) {
       destDir = await this.cloneCode(
         host,
         node.getKubeConfigPath(),
@@ -308,7 +308,7 @@ export default class StartDevModeCommand implements ICommand {
         node.resourceType,
         containerName
       );
-    } else if (result === nls['bt.open.dir']) {
+    } else if (result === nls["bt.open.dir"]) {
       const uris = await host.showOpenDialog({
         canSelectFiles: false,
         canSelectFolders: true,
@@ -337,26 +337,26 @@ export default class StartDevModeCommand implements ICommand {
     };
 
     const result = await host.showInformationMessage(
-      nls['tips.open'],
+      nls["tips.open"],
       { modal: true },
-      nls['bt.open.other'],
-      nls['bt.open.dir']
+      nls["bt.open.other"],
+      nls["bt.open.dir"]
     );
-    if (result === nls['bt.open.other']) {
+    if (result === nls["bt.open.other"]) {
       await getUrl();
-    } else if (result === nls['bt.open.dir']) {
+    } else if (result === nls["bt.open.dir"]) {
       destDir = await this.getAssociateDirectory();
 
       if (!existsSync(destDir)) {
         destDir = undefined;
 
         const res = await host.showInformationMessage(
-          'The directory does not exist, do you want to associate the new source code directory.',
+          "The directory does not exist, do you want to associate the new source code directory.",
           { modal: true },
-          'Associate'
+          "Associate"
         );
 
-        if (res === 'Associate') {
+        if (res === "Associate") {
           await getUrl();
         }
       }
@@ -421,7 +421,7 @@ export default class StartDevModeCommand implements ICommand {
 
     await vscode.window.withProgress(
       {
-        title: 'Starting DevMode',
+        title: "Starting DevMode",
         location: vscode.ProgressLocation.Notification,
         cancellable: false,
       },
@@ -430,9 +430,9 @@ export default class StartDevModeCommand implements ICommand {
           await node.setStatus(DeploymentStatus.starting);
           host.getOutputChannel().show(true);
           progress.report({
-            message: 'dev start',
+            message: "dev start",
           });
-          host.log('dev start ...', true);
+          host.log("dev start ...", true);
           let dirs: Array<string> | string = new Array<string>();
           let isOld = false;
           dirs = host.formalizePath(currentUri);
@@ -453,13 +453,13 @@ export default class StartDevModeCommand implements ICommand {
             node.getStorageClass(),
             node.getDevStartAppendCommand()
           );
-          host.log('dev start end', true);
-          host.log('', true);
+          host.log("dev start end", true);
+          host.log("", true);
 
           progress.report({
-            message: 'syncing file',
+            message: "syncing file",
           });
-          host.log('sync file ...', true);
+          host.log("sync file ...", true);
           await nhctl.syncFile(
             host,
             node.getKubeConfigPath(),
@@ -469,21 +469,21 @@ export default class StartDevModeCommand implements ICommand {
             node.resourceType,
             containerName
           );
-          host.log('sync file end', true);
-          host.log('', true);
-          node.setStatus('');
+          host.log("sync file end", true);
+          host.log("", true);
+          node.setStatus("");
           const parent = node.getParent();
           if (parent && parent.updateData) {
             await parent.updateData(true);
           }
-          await vscode.commands.executeCommand('Nocalhost.refresh', parent);
+          await vscode.commands.executeCommand("Nocalhost.refresh", parent);
 
           // await vscode.commands.executeCommand(EXEC, node);
           const terminal = await opendevSpaceExec(
             node.getAppName(),
             node.name,
             node.resourceType,
-            'nocalhost-dev',
+            "nocalhost-dev",
             node.getKubeConfigPath(),
             node.getNameSpace(),
             null
@@ -496,7 +496,7 @@ export default class StartDevModeCommand implements ICommand {
           );
         } catch (error) {
           logger.error(error);
-          node.setStatus('');
+          node.setStatus("");
         }
       }
     );
@@ -554,7 +554,7 @@ export default class StartDevModeCommand implements ICommand {
       workloadType,
       containerName
     );
-    let gitUrl = '';
+    let gitUrl = "";
     if (config) {
       gitUrl = config.dev.gitUrl;
     }
@@ -597,7 +597,7 @@ export default class StartDevModeCommand implements ICommand {
     if (!podName) {
       return;
     }
-    let containerName: string | undefined = (await node.getContainer()) || '';
+    let containerName: string | undefined = (await node.getContainer()) || "";
 
     if (!containerName) {
       const containerNameArr = await nhctl.getContainerNames({
