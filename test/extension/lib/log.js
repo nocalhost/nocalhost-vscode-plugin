87d0f12d84264f9dd5f1db830ec72572a339c01f
zhangjian <zhangjian@coding.net>
2021-09-14 20:02:48 +0800

    default level info

--- test/extension/lib/log.js
+++ test/extension/lib/log.js
@@ -2,7 +2,9 @@ const log4js = require("log4js");
 
 log4js.configure({
   appenders: { out: { type: "stdout" } },
-  categories: { default: { appenders: ["out"], level: "debug" } },
+  categories: {
+    default: { appenders: ["out"], level: process.env.LOGGER_LEVEL || "INFO" },
+  },
 });
 
 const logger = log4js.getLogger();
