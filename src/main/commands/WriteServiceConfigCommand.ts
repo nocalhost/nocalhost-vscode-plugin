e749d860eb5474fb1fe1c2719f21f5f9057be130
yuanganping <yuanganping@coding.net>
2021-10-27 14:13:40 +0800

    fix: del code

--- src/main/commands/WriteServiceConfigCommand.ts
+++ src/main/commands/WriteServiceConfigCommand.ts
@@ -1,11 +1,8 @@
 import * as vscode from "vscode";
-
 import ICommand from "./ICommand";
-import { WRITE_SERVICE_CONFIG } from "./constants";
+import { WRITE_SERVICE_CONFIG, EDIT_SERVICE_CONFIG } from "./constants";
 import registerCommand from "./register";
-import { getServiceConfig } from "../ctl/nhctl";
 import { ControllerResourceNode } from "../nodes/workloads/controllerResources/ControllerResourceNode";
-import host from "../host";
 
 export default class WriteServiceConfigCommand implements ICommand {
   command: string = WRITE_SERVICE_CONFIG;
@@ -13,36 +10,6 @@ export default class WriteServiceConfigCommand implements ICommand {
     registerCommand(context, this.command, false, this.execCommand.bind(this));
   }
   async execCommand(node: ControllerResourceNode) {
-    if (!node) {
-      host.showWarnMessage("Failed to get node configs, please try again.");
-      return;
-    }
-    let protocol = "NocalhostRW";
-    const svcProfile = await getServiceConfig(
-      node.getKubeConfigPath(),
-      node.getNameSpace(),
-      node.getAppName(),
-      node.name,
-      node.resourceType
-    );
-    if (
-      svcProfile.localconfigloaded ||
-      svcProfile.cmconfigloaded ||
-      svcProfile.annotationsconfigloaded
-    ) {
-      protocol = "Nocalhost";
-    }
-    const appNode = node.getAppNode();
-    const uri = vscode.Uri.parse(
-      `${protocol}://nh/config/app/${appNode.name}/services/${
-        node.name
-      }.yaml?appName=${node.getAppName()}&nodeName=${node.name}&resourceType=${
-        node.resourceType
-      }&workloadType=${
-        node.resourceType
-      }&id=${node.getNodeStateId()}&kubeConfigPath=${node.getKubeConfigPath()}&namespace=${node.getNameSpace()}`
-    );
-    let doc = await vscode.workspace.openTextDocument(uri);
-    vscode.window.showTextDocument(doc, { preview: true });
+    vscode.commands.executeCommand(EDIT_SERVICE_CONFIG, node);
   }
 }
