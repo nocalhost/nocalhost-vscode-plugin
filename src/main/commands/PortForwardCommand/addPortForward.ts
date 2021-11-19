c9045333e783a0d9ce6e2fa0f9398e818fee3314
zhangjian <zhangjian@coding.net>
2021-11-19 15:33:03 +0800

    style: Merge port forward
    Signed-off-by: zhangjian <zhangjian@coding.net>

---
+++ src/main/commands/PortForwardCommand/addPortForward.ts
@@ -0,0 +1,110 @@
+import * as vscode from "vscode";
+
+import host from "../../host";
+import { KubernetesResourceNode } from "../../nodes/abstract/KubernetesResourceNode";
+import { Deployment } from "../../nodes/workloads/controllerResources/deployment/Deployment";
+import { StatefulSet } from "../../nodes/workloads/controllerResources/statefulSet/StatefulSet";
+import * as nhctl from "../../ctl/nhctl";
+import { ControllerResourceNode } from "../../nodes/workloads/controllerResources/ControllerResourceNode";
+import { Pod } from "../../nodes/workloads/pod/Pod";
+
+const placeHolder = "single: 1234:1234 multiple: 1234:1234,2345:2345.";
+
+function validateInput(node: KubernetesResourceNode, value: string) {
+  const expReg = /^([1-9][0-9]*)?:([1-9][0-9]*)(,([1-9][0-9]*)?:([1-9][0-9]*))*$/;
+  const match = expReg.exec(value);
+
+  if (!match) {
+    return "please input correct string; example: " + placeHolder;
+  }
+
+  const errTip = "the number of port must be less than 65536";
+
+  function check(port: string | null) {
+    if (!port) {
+      return true;
+    }
+    return port && Number(port) < 65536;
+  }
+
+  const checkPort = new Array<string>();
+
+  if (node instanceof Deployment || node instanceof StatefulSet) {
+    checkPort.push(match[1], match[2], match[4], match[5]);
+  } else {
+    checkPort.push(match[1], match[2]);
+  }
+
+  const errors = checkPort.filter((value) => {
+    return !check(value);
+  });
+
+  if (errors.length > 0) {
+    return errTip;
+  }
+
+  return undefined;
+}
+
+async function getPodName(node: KubernetesResourceNode) {
+  let podName: string | undefined;
+  const svcProfile = await nhctl.getServiceConfig(
+    node.getKubeConfigPath(),
+    node.getNameSpace(),
+    node.getAppName(),
+    node.name,
+    node.resourceType
+  );
+
+  if (svcProfile?.develop_status !== "STARTED") {
+    if (node instanceof ControllerResourceNode) {
+      const kind = node.resourceType;
+      const name = node.name;
+
+      const podNameArr = await nhctl.getRunningPodNames({
+        name,
+        kind,
+        namespace: node.getNameSpace(),
+        kubeConfigPath: node.getKubeConfigPath(),
+      });
+
+      podName = podNameArr[0];
+
+      if (podNameArr.length > 1) {
+        podName = await vscode.window.showQuickPick(podNameArr);
+      }
+    } else if (node instanceof Pod) {
+      podName = node.name;
+    } else {
+      host.showInformationMessage("Does not support this type at the moment.");
+      return Promise.reject();
+    }
+  }
+  return podName;
+}
+export async function addPortForward(node: KubernetesResourceNode) {
+  const podName = await getPodName(node);
+
+  const ports = await vscode.window.showInputBox({
+    placeHolder,
+    validateInput: validateInput.bind(null, node),
+  });
+
+  if (!ports) {
+    return;
+  }
+
+  await nhctl.startPortForward(
+    host,
+    node.getKubeConfigPath(),
+    node.getNameSpace(),
+    node.getAppName(),
+    node.name,
+    "manual",
+    node.resourceType,
+    ports.split(","),
+    podName
+  );
+
+  host.showInformationMessage("Started Port Forward");
+}
---
+++ src/main/commands/PortForwardCommand/index.ts
@@ -0,0 +1,102 @@
+import * as vscode from "vscode";
+
+import host from "../../host";
+import { KubernetesResourceNode } from "../../nodes/abstract/KubernetesResourceNode";
+import { AppNode } from "../../nodes/AppNode";
+import { PORT_FORWARD } from "../constants";
+import ICommand from "../ICommand";
+import registerCommand from "../register";
+import { addPortForward } from "./addPortForward";
+import { getAppPortForwardList, getPortForwardList } from "./portForwardList";
+import * as nhctl from "../../ctl/nhctl";
+
+export default class PortForwardCommand implements ICommand {
+  command: string = PORT_FORWARD;
+  node: KubernetesResourceNode;
+
+  constructor(context: vscode.ExtensionContext) {
+    registerCommand(context, this.command, false, this.execCommand.bind(this));
+  }
+  async execCommand(node: KubernetesResourceNode) {
+    if (!node) {
+      host.showWarnMessage("Failed to get node configs, please try again.");
+      return;
+    }
+    this.node = node;
+
+    if (node instanceof AppNode) {
+      this.stopPortForward(await getAppPortForwardList(node));
+    } else {
+      this.showQuickPick(node);
+    }
+  }
+  async showQuickPick(node: KubernetesResourceNode) {
+    const pick = vscode.window.createQuickPick();
+    pick.placeholder = "Find port forward";
+    pick.items = [
+      { label: "$(add) Add port forward", alwaysShow: true },
+      ...(await getPortForwardList(node)),
+    ];
+
+    pick.onDidHide(() => {
+      pick.dispose();
+    });
+
+    pick.onDidChangeSelection(async (e) => {
+      const item = e[0];
+
+      if (item.label === "$(add) Add port forward") {
+        addPortForward(node);
+      } else {
+        this.stopPortForward(item);
+      }
+      pick.hide();
+    });
+
+    pick.show();
+  }
+
+  async stopPortForward(item: vscode.QuickPickItem | nhctl.IPortForward) {
+    if (!item) {
+      return;
+    }
+
+    let port: string;
+
+    let { namespace, appName, name, resourceType } = this.node;
+
+    if ("label" in item) {
+      port = item.label;
+    } else {
+      port = item.port;
+      appName = name;
+      name = item.svcName;
+      resourceType = item.servicetype;
+    }
+
+    const confirm = await vscode.window.showInformationMessage(
+      `Do you want to stop port-forward ${port}?`,
+      { modal: true },
+      "Confirm"
+    );
+
+    if (confirm !== "Confirm") {
+      return;
+    }
+
+    await nhctl.endPortForward({
+      namespace,
+      kubeConfigPath: this.node.getKubeConfigPath(),
+      appName,
+      workloadName: name,
+      port,
+      resourceType,
+    });
+
+    await vscode.commands.executeCommand("Nocalhost.refresh", this.node.parent);
+
+    host.showInformationMessage(`Ended Port Forward ${port}`);
+
+    host.getOutputChannel().show(true);
+  }
+}
