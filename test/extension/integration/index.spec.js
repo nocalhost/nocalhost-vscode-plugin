7c4cb84d9a6e87fd0cf28a9bf12a885725b274fa
zhangjian <zhangjian@coding.net>
2022-01-05 14:50:34 +0800

    WIP
    Signed-off-by: zhangjian <zhangjian@coding.net>

--- test/extension/integration/index.spec.js
+++ test/extension/integration/index.spec.js
@@ -9,16 +9,18 @@ const { devModeTests } = require("./devMode.test");
 
 const screenshotPath = path.join(__dirname, "../../../.screenshot");
 
-beforeEach(async () => {
-  const { currentTest } = jasmine;
-
-  logger.info("before " + currentTest.fullName);
-});
-
 afterEach(async () => {
   const { currentTest } = jasmine;
 
-  logger.info("after " + currentTest.fullName);
+  if (currentTest.failedExpectations.length > 0) {
+    logger.error(
+      currentTest.fullName,
+      "failure",
+      currentTest.failedExpectations[0].stack
+    );
+  } else {
+    logger.info(currentTest.fullName, "success");
+  }
 
   await page.screenshot({
     type: "jpeg",
@@ -32,6 +34,7 @@ describe("connect", connectTests);
 describe("install", installTests);
 describe("portForward", portForwardTests);
 describe("devMode", devModeTests);
+
 module.exports = {
   screenshotPath,
 };
