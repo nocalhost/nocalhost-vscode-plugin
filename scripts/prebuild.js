dc3ef5ed942ec5e02d78e3cab1933e83f4d8de5b
zhangjian <zhangjian@coding.net>
2022-01-14 11:11:29 +0800

    WIP: build version
    Signed-off-by: zhangjian <zhangjian@coding.net>

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,53 +1,54 @@
 const fs = require("fs");
-const path = require("path");
 const { execSync } = require("child_process");
+const path = require("path");
+const semver = require("semver");
 
 const packageJsonUri = path.resolve(__dirname, "../package.json");
 const packageJson = JSON.parse(
   fs.readFileSync(packageJsonUri, { encoding: "utf8" })
 );
 
-const { NHCTL_VERSION, MINIMUNM_VERSION_REQUIREMENT } = process.env;
+const { VERSION, NHCTL_VERSION, MINIMUNM_VERSION_REQUIREMENT } = process.env;
 
-let version = process.env.VERSION;
+// release
+if (VERSION) {
+  const version = semver.valid(VERSION);
 
-if (version) {
-  console.log("> update the version to: ", version);
-
-  const matched = version.match(/\d+\.\d+\.\d+/);
-  if (!matched || matched.length !== 1) {
-    return;
-  }
-
-  version = matched[0];
-
-  packageJson.version = version;
-  packageJson.nhctl.version = version;
-
-  require("./updateChangelog");
-} else {
-  let env = "alpha";
-  let version = packageJson.version;
-
-  if (process.env.CI === "true") {
-    env = "beta";
-
-    const rev = execSync(`git rev-parse --short HEAD`)
-      .toString()
-      .split("\n")[0];
-
-    packageJson.version = `${version}-${rev}-${env}`;
-  } else {
-    version = execSync(`git describe --tags --always --dirty="-${env}"`)
-      .toString()
-      .split("\n")[0];
+  if (version) {
+    console.log("> update the version to: ", version);
 
     packageJson.version = version;
+    packageJson.nhctl.version = version;
+
+    require("./updateChangelog");
+  } else {
+    throw Error(`version Invalid: ${VERSION}`);
   }
+} else {
+  //build
+  let version = execSync(`git describe --tags --abbrev=0`)
+    .toString()
+    .split("\n")[0];
+
+  version = semver.coerce(version);
+
+  version = semver.minVersion(`>${version}`);
+
+  const rev = execSync(`git rev-parse --short HEAD`).toString().split("\n")[0];
+
+  const identifier = process.env.CI === "true" ? "beta" : "alpha";
+
+  packageJson.version = `${version}-${identifier}.${rev}`;
 }
 
 if (MINIMUNM_VERSION_REQUIREMENT) {
-  packageJson.nhctl.serverVersion = MINIMUNM_VERSION_REQUIREMENT;
+  const version = semver.valid(MINIMUNM_VERSION_REQUIREMENT);
+
+  if (version) {
+    packageJson.nhctl.serverVersion = version;
+  } else {
+    throw Error(`serverVersion Invalid: ${MINIMUNM_VERSION_REQUIREMENT}`);
+  }
 }
 
 if (NHCTL_VERSION) {
