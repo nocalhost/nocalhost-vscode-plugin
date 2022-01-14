771e641c4ee920d69845c50f2a9a52ae837366ad
yuanganping <yuanganping@coding.net>
2022-01-14 10:51:48 +0800

    fix: config
    Signed-off-by: yuanganping <yuanganping@coding.net>

--- test/extension/integration/editConfig.js
+++ test/extension/integration/editConfig.js
@@ -1,3 +1,6 @@
+const ncp = require("copy-paste");
+const yaml2json = require("js-yaml");
+const json2yaml = require("json2yaml");
 const { tree } = require("../lib/components");
 
 const treeItemPath = [
@@ -6,13 +9,53 @@ const treeItemPath = [
   "bookinfo",
   "Workloads",
   "Deployments",
-  "ratings",
+  "authors",
 ];
 
-async function editConfig() {
-  const ratings = tree.getItem(...treeItemPath);
-  const setting = ratings.$(".action-label[title='Port Forward']");
+async function editConfig(page) {
+  const authors = tree.getItem(...treeItemPath);
+  const setting = authors.$(".action-label[title='Port Forward']");
+  // copy config
+  await page.waitForTimeout(5000);
+  await page.keyboard.down("Escape");
+  await page.keyboard.up("Escape");
 
+  await page.keyboard.down("Meta");
+  await page.keyboard.down("A");
+  await page.keyboard.up("A");
+  await page.keyboard.down("C");
+  await page.keyboard.up("C");
+  await page.keyboard.up("Meta");
+  // const content = await navigator.clipboard.readText();
+  const content = ncp.paste();
+  const obj = yaml2json.load(content);
+  obj.containers[0].dev.hotReload = !obj.containers[0].dev.hotReload;
+
+  const str = json2yaml.stringify(obj);
+  ncp.copy(str);
+
+  await page.waitForTimeout(1000);
+
+  await page.keyboard.down("Meta");
+  await page.keyboard.press("A");
+  await page.keyboard.up("Meta");
+  await page.keyboard.press("Backspace");
+
+  await page.waitForTimeout(1000);
+
+  await page.keyboard.down("Meta");
+  await page.keyboard.press("V");
+
+  await page.keyboard.press("S");
+
+  await page.keyboard.up("Meta");
+
+  await page.waitForTimeout(5000);
+
+  const tabElement = await page.waitForSelector(
+    'div[aria-label="authors.yaml"]'
+  );
+  assert.ok(tabElement._remoteObject.description.indexOf("dirty") === -1);
   await setting.click();
 }
 
