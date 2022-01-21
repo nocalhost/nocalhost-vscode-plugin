6a85c2f855ffe2de8578a93b931e451cbba598c9
yuanganping <yuanganping@coding.net>
2022-01-21 16:29:13 +0800

    feat: add duplicate mode
    Signed-off-by: yuanganping <yuanganping@coding.net>

--- test/extension/integration/duplicateDevMode.js
+++ test/extension/integration/duplicateDevMode.js
@@ -1,5 +1,6 @@
-const { tree } = require("../lib/components");
+const { tree, dialog, file } = require("../lib/components");
 const logger = require("../lib/log");
+const { checkSyncCompletion } = require("./devMode");
 
 const treeItemPath = [
   "",
@@ -7,7 +8,7 @@ const treeItemPath = [
   "bookinfo",
   "Workloads",
   "Deployments",
-  "authors",
+  "ratings",
 ];
 
 async function startDev() {
@@ -17,9 +18,54 @@ async function startDev() {
   );
   await duplicateDev.click();
 
-  logger.info("duplicate ");
+  logger.info("duplicate start");
+  if ((await dialog.getActionTexts()).includes("Open another directory")) {
+    await dialog.selectAction("Open another directory");
+  } else {
+    await dialog.selectAction("Open associated directory");
+  }
+
+  await file.selectPath(process.env.currentPath);
+}
+
+async function startDuplicateComplete() {
+  await page.waitForFunction(
+    (name) => {
+      const nodes = Array.from(
+        document.querySelectorAll(
+          "#workbench\\.parts\\.sidebar .monaco-list-row[aria-level='6']"
+        )
+      );
+
+      const node = nodes.find(
+        (node) => node.querySelector(".label-name").innerText === name
+      );
+      if (!node) {
+        return;
+      }
+
+      node.click();
+
+      const isDevStart = !!node.querySelector(
+        `.custom-view-tree-node-item-icon[style$='dev_copy.svg");']`
+      );
+
+      const isDevEnd = !!node.querySelector(
+        `.actions [style$='dev_end.svg");']`
+      );
+
+      return isDevStart && isDevEnd;
+    },
+    { timeout: 300_000 },
+    "ratings"
+  );
+
+  await checkSyncCompletion();
+
+  logger.debug("Start Duplicate Development", "ok");
 }
 
 module.exports = {
   startDev,
+  startDuplicateComplete,
 };
--- test/extension/integration/duplicateDevMode.test.js
+++ test/extension/integration/duplicateDevMode.test.js
@@ -1,9 +1,17 @@
 const { checkReady } = require("./portForward");
+const { startDev, startDuplicateComplete } = require("./duplicateDevMode");
+const { runCommand, codeSync, endDevMode } = require("./devMode");
 
 const duplicateDevModeTests = () => {
   beforeAll(async (done) => {
     checkReady().then(done).catch(done.fail);
   });
+
+  it("start", startDev);
+  it("start  complete", startDuplicateComplete);
+  it("run command", runCommand);
+  it("code sync", codeSync);
+  it("end", endDevMode);
 };
 
 module.exports = {
