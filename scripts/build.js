2e09f0eac14a092ba5c172cfd563adf717548dba
zhangjian <zhangjian@coding.net>
2022-02-16 17:28:09 +0800

    WIP
    Signed-off-by: zhangjian <zhangjian@coding.net>

---
+++ scripts/build.js
@@ -0,0 +1,93 @@
+const { spawn } = require("child_process");
+const path = require("path");
+const iconv = require("iconv-lite");
+const fs = require("fs");
+const fe = require("fs-extra");
+
+let argv = process.argv.slice(2);
+
+process.env.NODE_ENV = getArg("env") ?? "development";
+
+/**
+ *
+ * @param {String} str
+ */
+function getArg(str) {
+  const name = `--${str}=`;
+  let index = argv.findIndex((str) => str.startsWith(name));
+  if (index > -1) {
+    return argv.splice(index, 1)[0].replace(name, "");
+  }
+}
+/**
+ *
+ * @param {Array} args
+ */
+function getBuildArgs(args) {
+  args.push(
+    "--outdir=dist",
+    "--bundle",
+    `--define:process.env.NODE_ENV=\\"${process.env.NODE_ENV}\\"`,
+    ...argv
+  );
+
+  if (process.env.NODE_ENV === "development") {
+    args.push("--sourcemap", "--watch");
+  } else {
+    args.push("--minify");
+  }
+
+  return args;
+}
+/**
+ *
+ * @param {Array} args
+ */
+function startBuild(args) {
+  const proc = spawn(
+    path.resolve("./node_modules/.bin/esbuild"),
+    getBuildArgs(args),
+    {
+      shell: true,
+    }
+  );
+
+  proc.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
+  proc.stderr.on("data", (data) => {
+    process.stderr.write(data);
+  });
+}
+
+function extension() {
+  startBuild([
+    "src/main/extension.ts",
+    "--external:vscode",
+    "--format=cjs",
+    "--platform=node",
+    "--target=node14",
+  ]);
+}
+
+function html() {
+  fs.createReadStream("src/renderer/assets/fonts/DroidSansMono_v1.ttf").pipe(
+    fs.createWriteStream("dist/DroidSansMono_v1.ttf")
+  );
+
+  startBuild([
+    "atom-one-light=src/renderer/assets/css/atom-one-light.css",
+    "vs2015=src/renderer/assets/css/vs2015.css",
+    "renderer_v1=src/renderer/index.tsx",
+    "home=src/renderer/HomeIndex.tsx",
+    "--format=esm",
+    "--splitting",
+    "--target=chrome90",
+  ]);
+}
+
+fe.rmSync("dist", { recursive: true, force: true });
+fe.mkdirSync("dist");
+
+extension();
+html();
--- test/extension/integration/nhctl.js
+++ test/extension/integration/nhctl.js
@@ -1,13 +1,16 @@
 const { waitForMessage } = require("./index");
 const path = require("path");
 const { homedir } = require("os");
-const shell = require("shelljs");
+const shellWhich = require("which");
 
 /**
  * @param {puppeteer.Page} page
  */
 async function download() {
-  const result = shell.which(path.resolve(homedir(), ".nh", "bin", "nhctl"));
+  const result = !!shellWhich.sync(
+    path.resolve(homedir(), ".nh", "bin", "nhctl"),
+    { nothrow: true }
+  );
   if (result && result.code === 0) {
     return;
   }
