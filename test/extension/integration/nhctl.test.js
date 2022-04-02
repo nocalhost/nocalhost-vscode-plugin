1163816d955559d19ea2cbffcc1295b4853d3d2d
zhangjian <zhangjian@coding.net>
2022-04-02 12:10:01 +0800

    test: fix nhctl download fail
    Signed-off-by: zhangjian <zhangjian@coding.net>

--- test/extension/integration/nhctl.test.js
+++ test/extension/integration/nhctl.test.js
@@ -3,13 +3,14 @@ const { download } = require("./nhctl");
 const nhctlTests = () => {
   it(
     "download",
-    () =>
-      download.catch((err) => {
+    () => {
+      return download.catch((err) => {
         setTimeout(() => {
           process.kill(process.pid);
         }, 1_000);
         throw err;
-      }),
+      });
+    },
     10 * 60 * 1000
   );
 };
