23c68d324927af3e453142b66c39943ddb3134fc
zhangjian <1018zhangjian@gmail.com>
2022-05-19 22:47:14 +0800

    WIP
    Signed-off-by: zhangjian <1018zhangjian@gmail.com>

--- test/extension/lib/log.js
+++ test/extension/lib/log.js
@@ -1,7 +1,7 @@
 const log4js = require("log4js");
 
 log4js.configure({
-  appenders: { out: { type: "stdout" } },
+  appenders: { out: { type: "stdout" }, layout: { type: "basic" } },
   categories: {
     default: { appenders: ["out"], level: process.env.LOGGER_LEVEL || "INFO" },
   },
