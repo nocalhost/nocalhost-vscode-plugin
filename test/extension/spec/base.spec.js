29b4514a90dfb42a2af98b49b3b22ac2d98c693a
zhangjian <1018zhangjian@gmail.com>
2022-05-23 22:36:34 +0800

    WIP
    Signed-off-by: zhangjian <1018zhangjian@gmail.com>

---
+++ test/extension/spec/base.spec.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const logger = require("../lib/log");
+const { gitCode, getRepository } = require("../lib");
+
+const { nhctlTests } = require("../integration/nhctl.test");
+const { connectTests } = require("../integration/connect.test");
+
+const screenshotPath = path.join(__dirname, "../../../.screenshot");
+
+beforeAll(async (done) => {
+  gitCode(getRepository("bookinfo.git"))
+    .then((res) => {
+      process.env.tmpDir = res.tmpDir;
+      done();
+    })
+    .catch(done.fail);
+});
+
+afterEach(async () => {
+  const { currentTest } = jasmine;
+
+  if (currentTest.failedExpectations.length > 0) {
+    logger.error(
+      currentTest.fullName,
+      "failure",
+      currentTest.failedExpectations[0].stack
+    );
+  } else {
+    logger.info(currentTest.fullName, "success");
+  }
+
+  await page.screenshot({
+    type: "jpeg",
+    path: `${screenshotPath}/${currentTest.fullName}.jpeg`,
+    quality: 60,
+  });
+});
+
+describe("nhctl", nhctlTests);
+describe("connect", connectTests);
